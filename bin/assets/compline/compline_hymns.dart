454d9f60260635b3771a395d96968162b70b793e
franzoom <francois@labadens.me>
2025-07-08 23:47:57 +0200

    antiennes et invitatoires des laudes

--- bin/assets/compline/compline_hymns.dart
+++ bin/assets/compline/compline_hymns.dart
@@ -1,10 +1,10 @@
-import '../../classes/hymns_class.dart';
+import "../../classes/hymns_class.dart";
 
 final Map<String, Hymns> complineHymnsContent = {
-  'te-lucis-ante-terminum': Hymns(
-    title: 'Te lucis ante terminum',
-    author: '',
-    content: '''
+  "te-lucis-ante-terminum": Hymns(
+    title: "Te lucis ante terminum",
+    author: "",
+    content: """
 Te lucis ante términum,
 rerum creátor, póscimus,
 ut sólita cleméntia
@@ -24,117 +24,117 @@ Præsta, Pater omnípotens,
 per Iesum Christum Dóminum,
 qui tecum in perpétuum
 regnat cum Sancto Spíritu. Amen.
-''',
+""",
   ),
-  'en_toi_seigneur': Hymns(
-    title: 'En toi Seigneur, nos vies reposent',
-    author: 'CFC — CNPL',
-    content: '''
+  "en_toi_seigneur": Hymns(
+    title: "En toi Seigneur, nos vies reposent",
+    author: "CFC — CNPL",
+    content: """
 En toi Seigneur, nos vies reposent
-Et prennent force dans la nuit ;
+Et prennent force dans la nuit ;
 Tu nous prépares à ton aurore
-Et tu nous gardes dans l'Esprit.
+Et tu nous gardes dans l’Esprit.
 
-Déjà levé sur d'autres terres,
-Le jour éveille les cités ;
+Déjà levé sur d’autres terres,
+Le jour éveille les cités ;
 Ami des hommes, vois leur peine
-Et donne-leur la joie d'aimer.
+Et donne-leur la joie d’aimer.
 
 Vainqueur du mal et des ténèbres,
 Ô Fils de Dieu ressuscité,
-Délivre-nous de l'adversaire
-Et conduis-nous vers ta clarté !
-''',
+Délivre-nous de l’adversaire
+Et conduis-nous vers ta clarté !
+""",
   ),
-  'vienne_la_nuit_de_dieu': Hymns(
-    title: 'Vienne la nuit de Dieu',
-    author: 'P. Griolet — Levain',
-    content: '''
+  "vienne_la_nuit_de_dieu": Hymns(
+    title: "Vienne la nuit de Dieu",
+    author: "P. Griolet — Levain",
+    content: """
 R/ Vienne la nuit de Dieu,
 Vienne la nuit des hommes,
 Vienne toute la paix,
-Ô nuit de Jésus Christ !
-Alléluia ! Amen !
+Ô nuit de Jésus Christ !
+Alléluia ! Amen !
 
-Toi que j'ai cherché, Seigneur,
+Toi que j’ai cherché, Seigneur,
 En ce jour,
-Toi que j'ai reçu,
-Donne-moi le repos de ce jour !
+Toi que j’ai reçu,
+Donne-moi le repos de ce jour !
 
-Toi que j'ai chanté, Seigneur,
+Toi que j’ai chanté, Seigneur,
 En ce jour,
-Toi que j'ai prié,
-Donne-moi le repos de ce jour !
+Toi que j’ai prié,
+Donne-moi le repos de ce jour !
 
-Toi que j'ai nié, Seigneur,
+Toi que j’ai nié, Seigneur,
 En ce jour,
-Toi que j'ai aimé,
-Donne-moi le repos de ce jour !
+Toi que j’ai aimé,
+Donne-moi le repos de ce jour !
 
 R/ Vienne la nuit de Dieu,
 Vienne la nuit des hommes,
 Vienne toute la paix,
-Ô nuit de Jésus Christ !
-Alléluia ! Amen !
-''',
+Ô nuit de Jésus Christ !
+Alléluia ! Amen !
+""",
   ),
-  'vienne_la_nuit_de_dieu_lent': Hymns(
-    title: 'Vienne la nuit de Dieu',
-    author: 'P. Griolet — Levain',
-    content: '''
+  "vienne_la_nuit_de_dieu_lent": Hymns(
+    title: "Vienne la nuit de Dieu",
+    author: "P. Griolet — Levain",
+    content: """
 R/ Vienne la nuit de Dieu,
 Vienne la nuit des hommes,
 Vienne toute la paix,
-Ô nuit de Jésus Christ !
+Ô nuit de Jésus Christ !
 
-Toi que j'ai cherché, Seigneur,
+Toi que j’ai cherché, Seigneur,
 En ce jour,
-Toi que j'ai reçu,
-Donne-moi le repos de ce jour !
+Toi que j’ai reçu,
+Donne-moi le repos de ce jour !
 
-Toi que j'ai chanté, Seigneur,
+Toi que j’ai chanté, Seigneur,
 En ce jour,
-Toi que j'ai prié,
-Donne-moi le repos de ce jour !
+Toi que j’ai prié,
+Donne-moi le repos de ce jour !
 
-Toi que j'ai nié, Seigneur,
+Toi que j’ai nié, Seigneur,
 En ce jour,
-Toi que j'ai aimé,
-Donne-moi le repos de ce jour !
+Toi que j’ai aimé,
+Donne-moi le repos de ce jour !
 
 R/ Vienne la nuit de Dieu,
 Vienne la nuit des hommes,
 Vienne toute la paix,
-Ô nuit de Jésus Christ !
-''',
+Ô nuit de Jésus Christ !
+""",
   ),
-  'avant_la_fin_de_la_lumiere': Hymns(
-    title: 'Avant la fin de la lumière',
-    author: 'DP — AELF',
-    content: '''
+  "avant_la_fin_de_la_lumiere": Hymns(
+    title: "Avant la fin de la lumière",
+    author: "DP — AELF",
+    content: """
 Avant la fin de la lumière,
 Nous te prions, Dieu créateur,
 Pour que, fidèle à ta bonté,
 Tu nous protèges, tu nous gardes.
 
-Que loin de nous s'enfuient les songes,
+Que loin de nous s’enfuient les songes,
 Et les angoisses de la nuit.
-Préserve-nous de l'ennemi :
+Préserve-nous de l’ennemi :
 Que ton amour sans fin nous garde.
 
 Exauce-nous, Dieu, notre Père,
 Par Jésus Christ, notre Seigneur,
-Dans l'unité du Saint-Esprit,
+Dans l’unité du Saint-Esprit,
 Régnant sans fin dans tous les siècles.
-''',
+""",
   ),
-  'l_heure_s_avance_fais_nous_grace': Hymns(
-    title: "L'heure s'avance : fais-nous grâce",
-    author: 'C. Duchesneau — CNPL',
-    content: '''
-L'heure s'avance : fais-nous grâce,
-Toi dont le jour n'a pas de fin.
-Reste avec nous quand tout s'efface,
+  "l_heure_s_avance_fais_nous_grace": Hymns(
+    title: "l’heure s’avance : fais-nous grâce",
+    author: "C. Duchesneau — CNPL",
+    content: """
+l’heure s’avance : fais-nous grâce,
+Toi dont le jour n’a pas de fin.
+Reste avec nous quand tout s’efface,
 Dieu des lumières sans déclin.
 
 Tu sais toi-même où sont nos peines :
@@ -142,57 +142,57 @@ Porte au Royaume nos travaux.
 Sans toi, notre œuvre serait vaine :
 Viens préparer les temps nouveaux.
 
-Comme un veilleur attend l'aurore,
+Comme un veilleur attend l’aurore,
 Nous appelons le jour promis.
 Mais si la nuit demeure encore,
 Tiens-nous déjà pour tes amis.
 
 Dieu qui sans cesse nous enfantes,
-À toi ces derniers mots du jour !
-L'Esprit du Christ en nous les chante
+À toi ces derniers mots du jour !
+l’Esprit du Christ en nous les chante
 Et les confie à ton amour.
-''',
+""",
   ),
-  'ferme_mes_yeux': Hymns(
-    title: 'Ferme mes yeux pour revoir tes merveilles',
-    author: 'J.F Frié — Chalet',
-    content: '''
+  "ferme_mes_yeux": Hymns(
+    title: "Ferme mes yeux pour revoir tes merveilles",
+    author: "J.F Frié — Chalet",
+    content: """
 Ferme mes yeux pour revoir tes merveilles
-en ce moment que le jour fuit !
+en ce moment que le jour fuit !
 Allume dans la nuit
-une clarté nouvelle !
+une clarté nouvelle !
 
 Que le silence alentour me console
 de la faiblesse de ma foi,
-puisque j'écoute en moi
-résonner ta parole !
+puisque j’écoute en moi
+résonner ta parole !
 
-Jusqu'à demain, si se lève l'aurore,
-je t'abandonne mon esprit !
+Jusqu’à demain, si se lève l’aurore,
+je t’abandonne mon esprit !
 Ta grâce me suffit,
-c'est elle que j'implore.
-''',
+c’est elle que j’implore.
+""",
   ),
-  'nous-te-saluons-vierge-marie': Hymns(
-    title: 'Nous te saluons, Vierge Marie',
-    author: '',
-    content: '''
+  "nous-te-saluons-vierge-marie": Hymns(
+    title: "Nous te saluons, Vierge Marie",
+    author: "",
+    content: """
 Nous te saluons, Vierge Marie,
 servante du Seigneur.
 Ta foi nous a donné
-l'Enfant de la promesse,
+l’Enfant de la promesse,
 la source de la vie.
 Ève nouvelle,
 montre-nous le Sauveur,
 Jésus Christ, notre frère,
 Sainte Mère de Dieu.
-''',
+""",
   ),
-  'salve-regina': Hymns(
-    title: 'Salve Regina, mater misericordiae',
-    author: '',
-    content: '''
-Salve, Regina, mater misericordiæ ;
+  "salve-regina": Hymns(
+    title: "Salve Regina, mater misericordiae",
+    author: "",
+    content: """
+Salve, Regina, mater misericordiæ ;
 vita dulcedo et spes nostra, salve.
 Ad te clamamus, exsules filii Evæ.
 Ad te suspiramus, gementes et flentes
@@ -204,42 +204,42 @@ ad nos converte.
 Et Iesum, benedictum fructum ventris tui,
 nobis post hoc exsilium ostende.
 Ô clemens, o pia, o dulcis Virgo Maria.
-''',
+""",
   ),
-  'sainte-mere-du-redempteur': Hymns(
-    title: 'Sainte Mère de notre Rédempteur',
-    author: '',
-    content: '''
+  "sainte-mere-du-redempteur": Hymns(
+    title: "Sainte Mère de notre Rédempteur",
+    author: "",
+    content: """
 Sainte Mère du Rédempteur
 Porte du ciel, toujours ouverte,
 Étoile de la mer,
 Viens au secours du peuple qui tombe
 et qui cherche à se relever.
-Tu as enfanté, ô merveille !
-celui qui t'a créée,
+Tu as enfanté, ô merveille !
+celui qui t’a créée,
 et tu demeures toujours vierge.
-Accueille le salut de l'ange Gabriel
+Accueille le salut de l’ange Gabriel
 et prends pitié de nous, pécheurs.
-''',
+""",
   ),
-  'salut-reine-des-cieux': Hymns(
-    title: 'Salut, reine des cieux! Salut, reine des anges!',
-    author: '',
-    content: '''
-Salut, Reine des cieux ! Salut, Reine des anges !
-Salut, Tige féconde ! Salut, Porte du ciel !
-Par toi, la lumière s'est levée sur le monde.
+  "salut-reine-des-cieux": Hymns(
+    title: "Salut, reine des cieux ! Salut, reine des anges !",
+    author: "",
+    content: """
+Salut, Reine des cieux ! Salut, Reine des anges !
+Salut, Tige féconde ! Salut, Porte du ciel !
+Par toi, la lumière s’est levée sur le monde.
 
 Réjouis-toi, Vierge glorieuse,
-belle entre toutes les femmes !
+belle entre toutes les femmes !
 Salut, splendeur radieuse :
 implore le Christ pour nous.
-''',
+""",
   ),
-  'alma-redemptoris-mater': Hymns(
-    title: 'Alma Redemptoris Mater',
-    author: '',
-    content: '''
+  "alma-redemptoris-mater": Hymns(
+    title: "Alma Redemptoris Mater",
+    author: "",
+    content: """
 Alma Redemptoris Mater,
 quæ pervia cæli porta manes, et stella maris,
 succurre cadenti, surgere qui curat, populo :
@@ -248,88 +248,88 @@ tuum sanctum Genitorem,
 Virgo prius ac posterius,
 Gabrielis ab ore sumens illud Ave,
 peccatorum miserere.
-''',
+""",
   ),
-  'heureuse-es-tu-vierge-marie': Hymns(
-    title: 'Heureuse es-tu, Vierge Marie',
-    author: '',
-    content: '''
-Heureuse es-tu, Vierge Marie !
+  "heureuse-es-tu-vierge-marie": Hymns(
+    title: "Heureuse es-tu, Vierge Marie",
+    author: "",
+    content: """
+Heureuse es-tu, Vierge Marie !
 Par toi, le salut est entré dans le monde.
 Comblée de gloire, tu te réjouis devant le Seigneur,
-tu cries de joie à l'ombre de ses ailes.
+tu cries de joie à l’ombre de ses ailes.
 Sainte Mère de Dieu,
 prie pour nous, pauvres pécheurs.
-''',
+""",
   ),
-  'sous-l-abri-de-ta-misericorde': Hymns(
-    title: "Sous l'abri de ta miséricorde",
-    author: '',
-    content: '''
-Sous l'abri de ta miséricorde,
+  "sous-l-abri-de-ta-misericorde": Hymns(
+    title: "Sous l’abri de ta miséricorde",
+    author: "",
+    content: """
+Sous l’abri de ta miséricorde,
 nous nous réfugions, Sainte Mère de Dieu.
 Ne méprise pas nos prières
-quand nous sommes dans l'épreuve,
+quand nous sommes dans l’épreuve,
 mais de tous les dangers
 délivre-nous toujours,
 Vierge glorieuse, Vierge bienheureuse.
-''',
+""",
   ),
-  'reine-du-ciel-rejouis-toi': Hymns(
-    title: 'Reine du ciel, réjouis-toi, alléluia',
-    author: '',
-    content: '''
+  "reine-du-ciel-rejouis-toi": Hymns(
+    title: "Reine du ciel, réjouis-toi, alléluia",
+    author: "",
+    content: """
 Reine du ciel, réjouis-toi, alléluia,
 car le Seigneur que tu as porté, alléluia,
-est ressuscité comme il l'avait dit, alléluia,
+est ressuscité comme il l’avait dit, alléluia,
 Reine du ciel, prie Dieu pour nous, alléluia.
-''',
+""",
   ),
-  'o-vierge-marie-quelle-joie': Hymns(
-    title: 'Ô Vierge Marie, quelle joie!',
-    author: '',
-    content: '''
-Ô Vierge Marie, quelle joie ! Alléluia !
-Celui que tu as un jour enfanté, alléluia !
-Ressuscité, s'en est allé, alléluia !
-Pour nous, prie le Seigneur Dieu, alléluia !
-''',
+  "o-vierge-marie-quelle-joie": Hymns(
+    title: "Ô Vierge Marie, quelle joie !",
+    author: "",
+    content: """
+Ô Vierge Marie, quelle joie ! Alléluia !
+Celui que tu as un jour enfanté, alléluia !
+Ressuscité, s’en est allé, alléluia !
+Pour nous, prie le Seigneur Dieu, alléluia !
+""",
   ),
-  'regina-caeli': Hymns(
-    title: 'Regina cæli, lætare, alleluia',
-    author: '',
-    content: '''
+  "regina-caeli": Hymns(
+    title: "Regina cæli, lætare, alleluia",
+    author: "",
+    content: """
 Regina cæli, lætare, alleluia,
    quia quem meruisti portare, alleluia,
-resurrexit sicut dixit, alleluia ;
+resurrexit sicut dixit, alleluia ;
    ora pro nobis Deum, alleluia.
-''',
+""",
   ),
-  'sub-tuum-prasidium': Hymns(
-    title: 'Sub tuum præsidium',
-    author: '',
-    content: '''
+  "sub-tuum-prasidium": Hymns(
+    title: "Sub tuum præsidium",
+    author: "",
+    content: """
 Sub tuum præsidium confugimus,
-sancta Dei Genetrix ;
+sancta Dei Genetrix ;
 nostras deprecationes ne despicias
 in necessitatibus
 sed a periculis cunctis libera nos semper,
 Virgo gloriosa et benedicta.
-''',
+""",
   ),
-  'ave-regina-caelorum': Hymns(
-    title: 'Ave Regina cælorum',
-    author: '',
-    content: '''
+  "ave-regina-caelorum": Hymns(
+    title: "Ave Regina cælorum",
+    author: "",
+    content: """
 Ave, Regina cælorum,
 ave, Domina angelorum,
 salve, radix, salve, porta,
 ex qua mundo lux est orta.
 
 Gaude, Virgo gloriosa,
-super omnes speciosa ;
+super omnes speciosa ;
 vale, o valde decora,
 et pro nobis Christum exora.
-''',
+""",
   ),
 };

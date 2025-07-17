ffa961cda50ce6c0b5d1d112f2c90a9c3f6042b9
François Labadens <124710681+franzoom@users.noreply.github.com>
2025-07-17 16:31:38 +0200

    mini-modif de classes

--- bin/classes/mass_class.dart
+++ bin/classes/mass_class.dart
@@ -1,10 +1,1 @@
-class Saints {
-  /// Cette classe sert définir la structure de données pour la fiche d'un Saint
-  String?
-      title; // titre général de la fête ('Bienheureux Grégoire X, Pape' par exemple)
-  String? description; // petite biographie proposée
-  String?
-      usage; // texte en plus pour donner des détails: "fête, mais solennité dans la ville" par exemple
-  List?
-      liturgicalCommons; // liste des communs proposés (pasteurs / religieux, ...)
-}
+class Masses {}

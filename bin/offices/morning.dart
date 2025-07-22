c398b7e6bae11946cc6a268d23be614d416e84f3
franzoom <francois@labadens.me>
2025-07-22 12:23:04 +0200

    ajout des temps de férie aux laudes

--- bin/offices/morning.dart
+++ bin/offices/morning.dart
@@ -18,48 +18,48 @@ Map<String, Morning> ferialMorningResolution(
   Morning ferialMorning = Morning(); // création de l'instance ferialMorning
   final calendarDay = calendar.getDayContent(date);
   final celebrationName = calendarDay?.defaultCelebration;
+  if (!detectFerialDays(celebrationName!)) {
+    // si ce n'est pas une fête de férie, retourner un ensemble vide
+    return {celebrationName: ferialMorning};
+  }
 
-  if (detectFerialDays(celebrationName!)) {
-    if (celebrationName.startsWith('OT')) {
-      //Si on est dans le temps ordinaire
-      if (celebrationName.contains('SUNDAY')) {
-        final int weekNumber = int.parse(
-            celebrationName[celebrationName.length - 1]); // numéro de semaine
+  if (celebrationName.startsWith('OT')) {
+    //Si on est dans le temps ordinaire
+    if (celebrationName.contains('SUNDAY')) {
+      final int weekNumber = int.parse(
+          celebrationName[celebrationName.length - 1]); // numéro de semaine
 
-        //récupération des infos d'un des 4 premiers dimanches
-        // (si on est un des 4 premiers, on prend tout, autrement on ajoutera
-        // les infos du dimanche dont un a fait le modulo)
-        final int referenceWeekNumber = ((weekNumber - 1) % 4) + 1;
-        final dataFile =
-            File('../assets/morning/data/OT_SUNDAY_$referenceWeekNumber');
-        String fileContent = dataFile.readAsStringSync();
-        final fileExtracted = jsonDecode(fileContent);
-        ferialMorning = Morning.fromJson(fileExtracted);
+      //récupération des infos d'un des 4 premiers dimanches
+      // auquel on ajoutera les infos du dimanche s'il est supérieur à 4
+      final int referenceWeekNumber = ((weekNumber - 1) % 4) + 1;
+      final dataFile =
+          File('../assets/morning/data/OT_SUNDAY_$referenceWeekNumber');
+      String fileContent = dataFile.readAsStringSync();
+      final fileExtracted = jsonDecode(fileContent);
+      ferialMorning = Morning.fromJson(fileExtracted);
 
-        if (weekNumber > 4) {
-          //si c'est un autre dimanche, on intègre les antiennes de ce dimanche aux données des 4 premières semaines
-          final auxFile =
-              File('./bin/assets/morning/data/OT_SUNDAY_$weekNumber');
-          String auxContent = auxFile.readAsStringSync();
-          var auxExtracted = jsonDecode(auxContent);
-          final sundayAuxData = Morning.fromJson(auxExtracted);
-          //récupération
-          ferialMorning.mergeWith(
-              sundayAuxData); // ajoute les champs de AuxData dans sundayData
-        }
-      } else {
-        // si c'est en semaine, on prend le modulo à 4 pour n'utiliser les données
-        // que des 4 premières semaines
-        List dayDatas = extractWeekAndDay(celebrationName, "OT");
-        int weekNumber = dayDatas[0];
-        int dayNumber = dayDatas[1];
-        final int referenceWeekNumber = ((weekNumber - 1) % 4) + 1;
-        final dataFile = File(
-            './bin/assets/morning/data/OT_${referenceWeekNumber}_$dayNumber.json');
-        String fileContent = dataFile.readAsStringSync();
-        final fileExtracted = jsonDecode(fileContent);
-        ferialMorning = Morning.fromJson(fileExtracted);
+      if (weekNumber > 4) {
+        //si c'est un autre dimanche, on intègre les antiennes de ce dimanche aux données des 4 premières semaines
+        final auxFile = File('./bin/assets/morning/data/OT_SUNDAY_$weekNumber');
+        String auxContent = auxFile.readAsStringSync();
+        var auxExtracted = jsonDecode(auxContent);
+        final sundayAuxData = Morning.fromJson(auxExtracted);
+        //récupération
+        ferialMorning.mergeWith(
+            sundayAuxData); // ajoute les champs de AuxData dans sundayData
       }
+    } else {
+      // si c'est en semaine, on prend le modulo à 4 pour n'utiliser les données
+      // que des 4 premières semaines
+      List dayDatas = extractWeekAndDay(celebrationName, "OT");
+      int weekNumber = dayDatas[0];
+      int dayNumber = dayDatas[1];
+      final int referenceWeekNumber = ((weekNumber - 1) % 4) + 1;
+      final dataFile = File(
+          './bin/assets/morning/data/OT_${referenceWeekNumber}_$dayNumber.json');
+      String fileContent = dataFile.readAsStringSync();
+      final fileExtracted = jsonDecode(fileContent);
+      ferialMorning = Morning.fromJson(fileExtracted);
     }
     // on termine en ajoutant le titre et les psaumes correspondants
     ferialMorning.celebrationGrade = calendarDay?.defaultPriority;
@@ -69,6 +69,14 @@ Map<String, Morning> ferialMorningResolution(
     ferialMorning.psalm1Ref = morningPsalmList?[0];
     ferialMorning.psalm2Ref = morningPsalmList?[1];
     ferialMorning.psalm3Ref = morningPsalmList?[2];
-  }
+    return {celebrationName: ferialMorning};
+  } // fin du traitement du Temps Ordinaire
+
+  //pour les autres temps liturgiques de Férie:
+  final dataFile = File('../assets/morning/data/$celebrationName');
+  String fileContent = dataFile.readAsStringSync();
+  final fileExtracted = jsonDecode(fileContent);
+  ferialMorning = Morning.fromJson(fileExtracted);
+
   return {celebrationName: ferialMorning};
 }

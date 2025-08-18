7929443513f8244af290e23da1b8b80807a5a3ea
franzoom <francois@labadens.me>
2025-08-18 10:37:12 +0200

    correction of the Doxology for the introduction of celebrations

--- lib/assets/fixed_texts_library.dart
+++ lib/assets/fixed_texts_library.dart
@@ -1,6 +1,6 @@
 final Map<String, String> fixedTexts = {
   "officeIntroduction":
-      "<p>R/ Dieu, viens à mon aide.<br>V/ Seigneur, à notre secours.</p><p>Gloire au Père, et au Fils, et au Saint-Esprit, au Dieu qui était et qui vient, pour les siècles des siècles.<br>Amen. (Alléluia.)</p>",
+      "<p>R/ Dieu, viens à mon aide.<br>V/ Seigneur, à notre secours.</p><p>Gloire au Père, et au Fils, et au Saint-Esprit,<br>au Dieu qui était et qui vient,<br>pour les siècles des siècles.<br>Amen. (Alléluia.)</p>",
   "invitatoryIntroduction":
       "<p>R/ Seigneur, ouvre mes lèvres.<br>V/ Et ma bouche publiera ta louange.</p>",
   "officeConclusion":

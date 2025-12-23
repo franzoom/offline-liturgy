423051224d23bb26201658cbc2c04ebed893d2ab
franzoom <francois@labadens.me>
2025-12-23 18:30:09 +0100

    tiny fixes

--- lib/tools/date_tools.dart
+++ lib/tools/date_tools.dart
@@ -9,15 +9,7 @@ DateTime dayShift(DateTime date, int shift) {
 /// Detects if a celebration is a ferial day
 /// Returns true if the celebration name starts with one of the ferial prefixes
 bool isFerialDay(String celebrationCode) {
-  if (celebrationCode == "christmas_26" ||
-      celebrationCode == "christmas_27" ||
-      celebrationCode == "christmas_28" ||
-      celebrationCode == "christmas_29" ||
-      celebrationCode == "christmas_30" ||
-      celebrationCode == "christmas_31") {
-    return false;
-  }
-  final prefixes = ['ot', 'advent', 'lent', 'christmas', 'easter'];
+  final prefixes = ['ot', 'advent', 'lent', 'christmas_feriale', 'easter'];
   return prefixes.any((prefix) => celebrationCode.startsWith(prefix));
 }
 

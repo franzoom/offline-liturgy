d4ff73b0b61ae987d449e07880c7063c6216c255
franzoom <francois@labadens.me>
2026-04-27 22:50:01 +0200

    tiny fix

--- lib/offline_liturgy.dart
+++ lib/offline_liturgy.dart
@@ -7,7 +7,7 @@ library;
 
 export 'tools/data_loader.dart';
 export 'tools/date_tools.dart';
-export 'tools/location_loader.dart';
+export 'calendar_management/location_loader.dart';
 export 'classes/location_class.dart';
 export 'classes/office_elements_class.dart';
 export 'classes/calendar_class.dart';
